6fd835e58b80cc4f715b890abcd1f3367fa9d637
JJ <jj@dot.com>
2026-03-23 03:00:50 +0000

    conflict fix with dream-project , not using local module

--- nodes/sam2_masker.py
+++ nodes/sam2_masker.py
@@ -35,7 +35,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
-from utility.sam.sam2_image_predictor import SAM2ImagePredictor
+from ..utility.sam.sam2_image_predictor import SAM2ImagePredictor
 
 
 class SAM2Masker:
